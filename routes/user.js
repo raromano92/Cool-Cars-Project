3dbf429dee06e759cd646a270759eeda6f953c6f
Robert Romano <bromano1992@gmail.com>
2022-06-15 19:13:46 -0400

    Added user login response

--- routes/user.js
+++ routes/user.js
@@ -14,19 +14,24 @@ const router = express.Router();
 // Routes
 /////////////////////////////////////////
 
-// The Signup Routes (Get => form, post => submit form)
+// SIGNUP BUTTON ON INDEX PAGE TAKE YOU TO SIGNUP PAGE
 router.get("/signup", (req, res) => {
   res.render("users/signup");
 });
 
-// The login Routes (Get => form, post => submit form)
+// TAKES YOU TO THE LOGIN PAGE
 router.get("/login", (req, res) => {
   res.render("users/login");
 });
 
+// LOGIN THEN REDIRECTED TO MAIN CARS PAGE
 router.post("/login", (req, res) => {
   res.redirect("/cars");
 });
+
+/*========================================
+		SIGNUP RESPONSE
+========================================*/
 
 router.post("/signup", async (req, res) => {
 	// encrypt password
@@ -47,6 +52,44 @@ router.post("/signup", async (req, res) => {
 	  });
   });
   
+
+/*========================================
+		LOGIN RESPONSE
+========================================*/
+router.post("/login", async (req, res) => {
+	// get the data from the request body
+	const { username, password } = req.body;
+	// search for the user
+	User.findOne({ username })
+	  .then(async (user) => {
+		// check if user exists
+		if (user) {
+		  // compare password
+		  const result = await bcrypt.compare(password, user.password);
+		  if (result) {
+			// store some properties in the session object
+			req.session.username = username;
+			req.session.loggedIn = true;
+			// redirect to cars page if successful
+			res.redirect("/cars");
+		  } else {
+			// error if password doesn't match
+			res.json({ error: "password doesn't match" });
+		  }
+		} else {
+		  // send error if user doesn't exist
+		  res.json({ error: "user doesn't exist" });
+		}
+	  })
+	  .catch((error) => {
+		// send error as json
+		console.log(error);
+		res.json({ error });
+	  });
+  });
+  
+
+
 //////////////////////////////////////////
 // Export the Router
 //////////////////////////////////////////
